da8c6107987eef39e4cd62abe253902c90cb1767
HanssenDev <41926589+HanssenDev@users.noreply.github.com>
2019-07-23 00:41:45 +0100

    added functions

--- js/app.js
+++ js/app.js
@@ -1,16 +1,101 @@
 /*
  * Create a list that holds all of your cards
  */
+
+// Cards
 const cards = document.querySelectorAll('.card');
+
+let cardFlipped = false;
+let firstCard, secondCard;
+let matchedCards = [];
+
+// Timer
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let t;
+
+const timer = document.querySelector(".timer");
+const hourTimer = document.querySelector(".hour");
+const minuteTimer = document.querySelector(".minute");
+const secondsTimer = document.querySelector(".seconds");
+
+let timeCounter;
+let timerOn = false;
+
 
 shuffle(cards);
 
+// Timer functions
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    
+    // h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+};
+
+function startTimer() {
+    t = setInterval(add, 1000);
+};
+
+
+// Card flipping functionality
+
+function flipCard() {
+    this.classList.add('open', 'show');
+
+    if (!cardFlipped) {
+        cardFlipped = true;
+        firstCard = this;
+        // start timer
+        if (timerOn === false) {
+            startTimer();
+            timerOn = true;
+        }
+    } else {
+        cardFlipped = false;
+        secondCard = this;
+        
+        // Remove event listeners if cards match
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+            matchedCards.push(firstCard);
+            matchedCards.push(secondCard);
+        } else {
+            // hide cards if they don't match
+            setTimeout( function() {
+                firstCard.classList.remove('open', 'show');
+                secondCard.classList.remove('open', 'show');
+            }, 1000);
+        }
+
+    }
+};
+
+// adding an Event Listener to each card
 
 cards.forEach(function(card) {
-    card.addEventListener('click', function(e) {
-        card.classList.add('open', 'show');
-    })
+    card.addEventListener('click', flipCard);
 });
+
+
+// Win game
+
+function gameWon() {
+    if (matchedCards.length === 16) {
+
+    }
+};
+
 
 
 
